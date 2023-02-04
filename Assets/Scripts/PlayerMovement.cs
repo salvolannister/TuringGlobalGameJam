0868e7f6048ad6da516f340dd5224f0b5d655e48
federico_gallucci97 <federico.gallucci.97@gmail.com>
2023-02-05 00:44:47 +0100

    implemented player animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using System;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     LevelManager levelManager;
     [SerializeField] private float movementSpeed = 3f;
     [SerializeField] private Transform movePoint;
-    [SerializeField][Tooltip("nome della traccia da inserire")] private String _eventName;
+    [SerializeField] [Tooltip("nome della traccia da inserire")] private String _eventName;
     [SerializeField] private Animator playerAnimator;
     private Vector2 lastMove;
 
@@ -14,7 +15,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask nonWalkableTiles;
 
     public bool blockMovement = false;
-    private bool isAnimating;
+    private Vector2 prevDir;
+    private float prevTime;
 
     // Start is called before the first frame update
 
@@ -29,32 +31,30 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= .05f && !blockMovement)
+        if (Vector3.Distance(transform.position, movePoint.position) <= .05f && !blockMovement )
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                if(CheckMovementPossible(new Vector2(Input.GetAxisRaw("Horizontal"), 0)))
+                if (CheckMovementPossible(new Vector2(Input.GetAxisRaw("Horizontal"), 0)))
                     PerformMovement(new Vector2(Input.GetAxis("Horizontal"), 0));
             }
-            else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                if(CheckMovementPossible(new Vector2(0, Input.GetAxisRaw("Vertical"))))
+                if (CheckMovementPossible(new Vector2(0, Input.GetAxisRaw("Vertical"))))
                     PerformMovement(new Vector2(0, Input.GetAxis("Vertical")));
             }
         }
 
-        if(isAnimating && !Input.anyKey)
+
+        if ((transform.position - movePoint.position).magnitude == 0)
         {
-            isAnimating = false;
             playerAnimator.SetBool("Up", false);
             playerAnimator.SetBool("Down", false);
             playerAnimator.SetBool("Left", false);
             playerAnimator.SetBool("Right", false);
         }
 
-
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementSpeed * Time.deltaTime);
-
     }
 
 
@@ -66,43 +66,50 @@ public class PlayerMovement : MonoBehaviour
         PlayerAnimation(movement);
         FMODUnity.RuntimeManager.PlayOneShot("event:/" + _eventName);
         levelManager.UpdateStepsEvenet();
-
+        prevTime = Time.time;
     }
 
 
     public bool CheckMovementPossible(Vector2 movement)
     {
-        if(Physics2D.OverlapCircle((Vector2)movePoint.position + movement, .1f, nonWalkableTiles))
+        if (Physics2D.OverlapCircle((Vector2)movePoint.position + movement, .1f, nonWalkableTiles))
             return false;
-        return true;
 
+        return true;
     }
 
     private void PlayerAnimation(Vector2 direction)
     {
-        if(isAnimating) return;
+        if (prevDir != direction)
+        {
+            playerAnimator.SetBool("Up", false);
+            playerAnimator.SetBool("Down", false);
+            playerAnimator.SetBool("Left", false);
+            playerAnimator.SetBool("Right", false);
+        }
 
-        if(direction == Vector2.up)
+        prevDir = direction;
+
+
+        if (direction == Vector2.up)
         {
             playerAnimator.SetBool("Up", true);
             Debug.Log("Walk Up");
         }
-        if(direction == Vector2.down)
+        if (direction == Vector2.down)
         {
             playerAnimator.SetBool("Down", true);
             Debug.Log("Walk Down");
         }
-        if(direction == Vector2.left)
+        if (direction == Vector2.left)
         {
             playerAnimator.SetBool("Left", true);
             Debug.Log("Walk Left");
         }
-        if(direction == Vector2.right)
+        if (direction == Vector2.right)
         {
             playerAnimator.SetBool("Right", true);
             Debug.Log("Walk Right");
         }
-
-        isAnimating = true;
     }
 }

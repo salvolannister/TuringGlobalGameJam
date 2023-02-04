7c5544e008ff34fd2e843f955b1e4bf53b39f042
Matt-Not <70361629+Matt-Not@users.noreply.github.com>
2023-02-04 16:01:54 +0100

    Lever update

--- Assets/Lever.cs
+++ Assets/Lever.cs
@@ -6,13 +6,26 @@ public class Lever : MonoBehaviour
 {
     [SerializeField] private GameObject gate;
     [SerializeField, Layer] private int defaultLayer;
+    [SerializeField, Layer] private int wallLayer;
+    [SerializeField] private bool open;
     // Start is called before the first frame update
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) {
-            gate.layer = defaultLayer;
-            gate.GetComponentInChildren<SpriteRenderer>().color = Color.white;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (open)
+            {
+                open = false;
+                gate.layer = wallLayer;
+                gate.GetComponentInChildren<SpriteRenderer>().color = Color.yellow;
+            }
+            else
+            {
+                open = true;
+                gate.layer = defaultLayer;
+                gate.GetComponentInChildren<SpriteRenderer>().color = Color.white;
+            }
         }
     }
 }

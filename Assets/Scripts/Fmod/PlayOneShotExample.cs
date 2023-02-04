1d749304bce9239fad88ed7a540100df2d82607f
Simone Marca <simone_marca@outlook.it>
2023-02-04 15:15:19 +0100

    Init Fmod plugin

---
+++ Assets/Scripts/Fmod/PlayOneShotExample.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOneShotExample : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            FMODUnity.RuntimeManager.PlayOneShot("event:/Footsteps");
+        }
+    }
+}

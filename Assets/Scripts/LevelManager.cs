6dcff514b2c5d50ccc0af48a32e32b039aab45ee
Simone Marca <simone_marca@outlook.it>
2023-02-04 12:44:31 +0100

    Add first logic on LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,13 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : Manager<LevelManager>
 {
+    private long _currentSteps;
+    private int _currentSceneIndex;
+    public UnityEvent _updateStepsEvent = new UnityEvent();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentSteps = 0;
+        _currentSceneIndex = 1;
+        _updateStepsEvent.AddListener(UpdateStepsEvenet);
     }
 
     // Update is called once per frame
@@ -15,4 +23,34 @@ public class LevelManager : Manager<LevelManager>
     {
         
     }
+    
+    //azzera numero di passi
+    //carica scena successiva
+    private void LoadNextScene()
+    {
+        ResetCurrentSteps();
+        _currentSceneIndex++;
+        SceneManager.LoadScene("Scena"+_currentSceneIndex, LoadSceneMode.Additive);           
+    }
+
+    //azzera numero di passi
+    //ricarica la scena corrente
+    private void ResetCurrentScene()
+    {
+        ResetCurrentSteps();
+        SceneManager.LoadScene("Scena" + _currentSceneIndex, LoadSceneMode.Additive);
+    }
+
+    private void ResetCurrentSteps()
+    {
+        _currentSteps = 0;
+    }
+
+    public void UpdateStepsEvenet()
+    {
+        _currentSteps++;
+        //Output message to the console
+        Debug.Log("passi aumentati");
+    }
 }
+

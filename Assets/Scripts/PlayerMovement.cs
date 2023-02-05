2c55ae742df6f4c696f4129678e7d03a62011213
Simone Marca <simone_marca@outlook.it>
2023-02-05 01:01:11 +0100

    Added oil

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 3f;
     [SerializeField] private Transform movePoint;
     [SerializeField][Tooltip("nome della traccia da inserire")] private String _eventName;
-    private Vector2 lastMove;
+    public Vector2 lastMove;
 
 
     [SerializeField] private LayerMask nonWalkableTiles;
@@ -28,23 +28,28 @@ public class PlayerMovement : MonoBehaviour
 
         if (Vector3.Distance(transform.position, movePoint.position) <= .05f && !blockMovement)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                if (CheckMovementPossible(new Vector2(Input.GetAxisRaw("Horizontal"), 0)))
-                    PerformMovement(new Vector2(Input.GetAxis("Horizontal"), 0));
-            }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-            {
-                if (CheckMovementPossible(new Vector2(0, Input.GetAxisRaw("Vertical"))))
-                    PerformMovement(new Vector2(0, Input.GetAxis("Vertical")));
-            }
+            CheckAndPerformMovement();
         }
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, movementSpeed * Time.deltaTime);
 
     }
 
+    public void CheckAndPerformMovement()
+    {
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+        {
+            if(CheckMovementPossible(new Vector2(Input.GetAxisRaw("Horizontal"), 0)))
+                PerformMovement(new Vector2(Input.GetAxis("Horizontal"), 0));
+        }
+        else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+        {
+            if(CheckMovementPossible(new Vector2(0, Input.GetAxisRaw("Vertical"))))
+                PerformMovement(new Vector2(0, Input.GetAxis("Vertical")));
+        }
+    }
 
-    public void PerformMovement(Vector2 movement)
+
+    private void PerformMovement(Vector2 movement)
     {
         lastMove = movement;
         Debug.Log(lastMove);
